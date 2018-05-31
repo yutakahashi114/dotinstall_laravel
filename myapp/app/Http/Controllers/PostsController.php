7db0bfbc43f00bfface579c3a97950384cc2a16b
高橋　祐羽 <yu_takahashi@inoue.inv.local>
2018-05-31 15:45:33 +0900

    11

--- myapp/app/Http/Controllers/PostsController.php
+++ myapp/app/Http/Controllers/PostsController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
 class PostsController extends Controller
 {
     //
     public function index() {
-        return 'hello';
+        // $posts = \App\Post::all();
+        // $posts = Post::all();
+        // $posts = Post::orderBy('create_at', 'desc')->get();
+        $posts = Post::latest()->get();
+        // dd($posts->toArray()); // dump die
+        return view('posts.index');
     }
 }
---
+++ myapp/resources/views/posts/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8"> 
+    <title>Blog Posts</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Blog Posts</h1>
+        <ul>
+            <li><a href="">title</a></li>
+            <li><a href="">title</a></li>
+            <li><a href="">title</a></li>
+        </ul>
+    </div>
+</body>
+</html>
