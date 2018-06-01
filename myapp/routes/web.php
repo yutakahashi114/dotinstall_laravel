440ea9832f6ca7d64e6712beba486b303a10bbdf
高橋　祐羽 <yu_takahashi@inoue.inv.local>
2018-06-01 10:55:17 +0900

    21

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -21,3 +21,4 @@ Route::get('/posts/{post}', 'PostsController@show')->where('post', '[0-9]+');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}/edit', 'PostsController@edit');
+Route::patch('/posts/{post}', 'PostsController@update');
